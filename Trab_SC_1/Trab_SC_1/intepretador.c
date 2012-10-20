1017653ff2983ba4abeb06f6eabfa39f9417ac88
Fabio <Mourth>
2012-10-20 18:23:01 -0300

    Em progresso
    Nõa etá funcionando ainda.

--- Trab_SC_1/Trab_SC_1/intepretador.c
+++ Trab_SC_1/Trab_SC_1/intepretador.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "interpretador.h"
 
+typedef struct _processo{
+	int PID;
+	int tempoUCP;
+	int nivelPrioridade;
+	int tempoEs;
+	int tempoExecucao;
+	int tempoEspera;
+} Processo;
 
 typedef struct _leitor{
 	int qtdComandos;
 	char** vetComandos;
 	int comandoAtual;
+	pProcesso vetProcesso ;
 } Leitor;
 
 
@@ -15,6 +25,7 @@ pLeitor criarLeitor()
 	pLeitor leitorRet = (pLeitor) malloc(sizeof(Leitor));
 	if(leitorRet != NULL)
 		return leitorRet;
+	
 	return NULL;
 }
 
@@ -31,7 +42,79 @@ int getComandoAtual(pLeitor pleitor)
 	return pleitor->comandoAtual;
 }
 
+char * getComandoN (pLeitor pleitor, int n)
+{
+	return pleitor->vetComandos[n];
+}
+
+//assumo que o leitor está criado e inicializado e o arquivo não é null e que o file é igual ao nome do arquivo
+int preencheCommandos (FILE * file, pLeitor pleitor)
+{
+	int numeroLinhas;
+	char * linhaDeComando;
+	
+	numeroLinhas = contaNumeroLinhasDoArquivo(file);
+	pleitor->vetComandos = (char **) malloc ( numeroLinhas*sizeof(char*));
+	if (pleitor->vetComandos == NULL)
+		return 0;
+
+	pleitor->vetProcesso = (Processo*) malloc (sizeof (Processo*));
+	if (pleitor->vetProcesso == NULL)
+		return 0;
+
+	/*
+
+	if ( strcmp (nomeDoArquivo, "SJF_comandos.txt"){
+		
+	}
+	if ( strcmp (nomeDoArquivo, "Prioridades_comandos.txt "){
+		
+	}
+	if ( strcmp (nomeDoArquivo, "FIFO_comandos.txt"){
+		
+	}
+	if ( strcmp (nomeDoArquivo, "RoudRobin_comandos.txt"){
+		
+	}
+
+
+
+	*/
+	while( fscanf(file,"%[^\n]",linhaDeComando) != EOF)
+	{
+		pleitor->vetComandos[pleitor->qtdComandos] = (char *) malloc (strlen(linhaDeComando)*sizeof(char));
+		if (pleitor->vetComandos[pleitor->qtdComandos] == NULL)
+			return 0;
+
+	// TODO preencher o processo
+
+		strcpy(pleitor->vetComandos[pleitor->qtdComandos],linhaDeComando);
+		pleitor->qtdComandos++;
+	}
+
+	return 1;
+}
+
+char* interarComando (pLeitor pleitor)
+{
+	pleitor->comandoAtual++;
+
+	if (pleitor->comandoAtual > pleitor->qtdComandos)
+		return NULL;
+
+	return pleitor->vetComandos[pleitor->comandoAtual - 1];
+}
+
 int getQtdComando(pLeitor pleitor)
 {
 	return pleitor->qtdComandos;
+}
+
+static int contaNumeroLinhasDoArquivo (FILE* file)
+{
+	int numeroLinhas = 0;
+	while ( fscanf(file,"%s") != EOF)
+		numeroLinhas++;
+
+	return numeroLinhas;
 }
--- Trab_SC_1/Trab_SC_1/interpretador.h
+++ Trab_SC_1/Trab_SC_1/interpretador.h
@@ -1,9 +1,14 @@
 typedef struct _leitor *  pLeitor ;
-
+typedef struct _processo * pProcesso ;
 
 pLeitor criarLeitor();
 
 void inicializaLeitor(pLeitor pLeitor);
 
 int getComandoAtual(pLeitor pleitor);
+
 int getQtdComando(pLeitor pleitor);
+
+int preencheCommandos (FILE * file, pLeitor pLeitor);
+
+char* interarComando (pLeitor pleitor);
