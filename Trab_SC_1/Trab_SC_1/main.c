2a7a24e1cb8830e262e9ba717a6990da1f2e2dc1
Felix Ribeiro <ribeiro.felix@live.com>
2012-10-26 04:37:40 -0200

    inicio roundrobin

--- Trab_SC_1/Trab_SC_1/main.c
+++ Trab_SC_1/Trab_SC_1/main.c
@@ -8,26 +8,27 @@ int main(){
 	pLeitor leitorSJF = criarLeitor();
 	pLeitor leitorPrioridades = criarLeitor();
 	pLeitor leitorRoudRobin = criarLeitor();
+	
 	pEscalonador pEscalonadorFifo = criaEscalonador( Fifo,0);
 	pEscalonador pEscalonadorSJF = criaEscalonador(SJF,0);
 	pEscalonador pEscalonadorPriopridades = criaEscalonador(Prioridades,0);
-	pEscalonador pEscalonadorRoundRobin = criaEscalonador(RoudRobin,0);
+	pEscalonador pEscalonadorRoundRobin = criaEscalonador(RoudRobin,5);
 	pProcesso pProcessoAInserir  ;
 	int comando = 5 ;
-
+	//printf("oi1");
 
 	FILE * entradaFifo = fopen("FIFO_comandos.txt","rt");
-	FILE * entradaSJF = fopen("SJF_comandos.txt","rt");
+	/*FILE * entradaSJF = fopen("SJF_comandos.txt","rt");
 	FILE * entradaPrioridades = fopen("Prioridades_comandos.txt","rt");
 	FILE * entradaRoudRobin = fopen("RoudRobin_comandos.txt","rt");
-	FILE * saida = fopen("E:\\Documento\\saida.txt","rt" );
+	FILE * saida = fopen("E:\\Documento\\saida.txt","rt" ); */
 
 	preencheCommandos(entradaFifo,leitorFifo,"FIFO_comandos.txt");
-	preencheCommandos(entradaSJF,leitorSJF,"SJF_comandos.txt");
+	/*preencheCommandos(entradaSJF,leitorSJF,"SJF_comandos.txt");
 	preencheCommandos(entradaPrioridades,leitorPrioridades,"Prioridades_comandos.txt");
 	preencheCommandos(entradaRoudRobin,leitorRoudRobin,"RoudRobin_comandos.txt");
 	
-
+*/
 	// Itera  sobre cada um dos leitores e depois executa os comandos.
 
 	//FIFO
@@ -48,7 +49,7 @@ int main(){
 		insereProcesso(pEscalonadorFifo,pProcessoAInserir);
 	}*/
 
-	while (comando != 0)
+	/*while (comando != 0)
 	{
 		pProcesso pProcesso = NULL;
 		if(comando%2)
@@ -81,10 +82,22 @@ int main(){
 		insereProcesso(pEscalonadorPriopridades,pProcesso);
 		comando--;
 	}
+*/
+	comando = 5 ;
+	while (comando != 0)
+	{
+		
+		pProcesso pProcesso = NULL;
+		printf("oi");
+		if(comando%2)
+			 pProcesso = criaProcesso(0,0,comando,0,comando,comando,"ls");
+		else
+			 pProcesso = criaProcesso(0,0,comando,0,comando,comando,"man");
+		insereProcesso(pEscalonadorRoundRobin,pProcesso);
+		comando--;
+	}
 
-
-
-	executaEscalonamento(pEscalonadorPriopridades);
+	executaEscalonamento(pEscalonadorRoundRobin);
 	
 	return 0;
 }
